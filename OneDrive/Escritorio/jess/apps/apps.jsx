c5c422fde690b33cca94ae58c58604b66771d757
agostina <cocobratti@hotmail.com>
2022-12-01 18:19:47 -0300

    corregir errores

---
+++ OneDrive/Escritorio/jess/apps/apps.jsx
@@ -0,0 +1,14 @@
+import navbar from "../componentes/navbar";
+
+
+
+function App () {
+    let styles = {
+        color: 'white',
+        backgroundColor: 'blue',
+    }
+return (
+    <navBar/>
+)
+
+}
